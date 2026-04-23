ffe3203c3d1fe8e13a7341506c895f67c2c69d3a
Praagya Shandilya <praagya.shandilya@outlook.com>
2026-04-23 00:02:41 -0400

    separated prompts and fixed pyproject.toml

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from typing import TypedDict, Annotated, List
 import operator
 import os
 import re
+from prompts import SYS_PROMPT
 import requests
 from pydantic import BaseModel, Field
 
@@ -11,18 +12,24 @@ from langgraph.checkpoint.sqlite import SqliteSaver
 from langchain_openrouter import ChatOpenRouter
 from langchain_core.messages import AnyMessage, SystemMessage, HumanMessage, AIMessage, ChatMessage
 
-
-
-class Procon(BaseModel):
-    """ The prompts to generate the for(pro) argument and against(con) argument  """
-    pro:str = Field(description="the prompt for the pro argument")
-    con:str = Field(description="the prompt for the con argument")
-
-
 memory = SqliteSaver.from_conn_string(":memory:")
 
 model_name = "moonshotai/kimi-k2.6"
 
+
+class AgentState(TypedDict):
+    pro:str
+    con:str
+    content:List[str]
+    pro_search_topics:List[str]
+    con_search_topics:List[str]
+
+class Procon(BaseModel):
+    """ The prompts to generate the for(pro) argument and against(con) argument  """
+    pro:str = Field(description="the prompt for the pro argument")
+    pro_search_topics: List[str] = Field(description="the search topics for the pro argument")
+    con:str = Field(description="the prompt for the con argument")
+    con_search_topics: List[str] = Field(description="the search topics for the con argument")
 
 model = ChatOpenRouter(
     model = model_name,
@@ -32,15 +39,15 @@ model = ChatOpenRouter(
 
 
 
-SYS_PROMPT = """
-For the following proposition, generate a positive prompt and a negative prompt for agents  to look into: 
-Is it justified to build compromised systems now for speed or build secure systems later?
-label your prompt as "PRO PROMPT:" and "CON PROMPT:"
-"""
+
+
+
+
+
 
 messages=[
     SystemMessage(content=SYS_PROMPT)
 ]
 
 response1 = model.with_structured_output(Procon).invoke(messages)
-print(response1.pro,"\n",response1.con)
+print(response1.pro,"\n","*"*20,"\n","*"*20,"\n",response1.con,"\n","*"*20,"\n","*"*20,"\n",response1.pro_search_topics,"\n","*"*20,"\n","*"*20,"\n",response1.con_search_topics)
---
+++ prompts.py
@@ -0,0 +1,6 @@
+SYS_PROMPT = """
+You are a researcher who is given the task of directing two agents, 
+the first agent finds the positives (pros) of the given task the second agent finds the negatives(cons) 
+as well as finding some more topics to be searched that would help the respective agents;
+label your prompts as "PRO PROMPT:" and "CON PROMPT:
+"""
